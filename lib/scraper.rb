c8dc2ace43a7f6987f4cd44afff1170690a4c930
Justin Breneman <justin.breneman@yahoo.com>
2020-07-13 21:28:49 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,19 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    index_page = []
+    doc = Nokogiri::HTML(open(index_url))
+    doc.css(".student-card").each do |student|
+      self.scrape_profile_page(student.css(".href"))
+    end
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    binding.pry
   end
 
 end
-
